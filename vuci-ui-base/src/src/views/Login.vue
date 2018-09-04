2540373eed2de8532c9530c07a205ed5d5d76be7
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-09-04 20:41:54 +0800

    Login.vue: Reimplemented by buefy
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/views/Login.vue
+++ vuci-ui-base/src/src/views/Login.vue
@@ -1,58 +1,60 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-alert type="error" :value="!login">Invalid username and/or password! Please try again.</v-alert>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Authorization Required</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <p>Please enter your username and password.</p>
-                <v-form ref="form">
-                  <v-text-field prepend-icon="person" v-model="username" :rules="rules.username" label="Username" type="text"></v-text-field>
-                  <v-text-field prepend-icon="lock" v-model="password" label="Password" type="password" @keyup.enter="submit"></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="submit">Login</v-btn>
-                <v-btn @click="reset">Reset</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+  <div class="card login">
+    <header class="card-header">
+      <p class="card-header-title">Authorization Required</p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <form ref="form">
+          <b-field label="Username" horizontal>
+            <b-input v-model="username"></b-input>
+          </b-field>
+          <b-field label="Password" horizontal>
+            <b-input type="password" v-model="password" password-reveal></b-input>
+          </b-field>
+          <b-field horizontal>
+            <p class="control">
+              <a class="button is-primary is-fullwidth" @click="submit">Login</a>
+            </p>
+            <p class="control">
+              <a class="button is-primary is-fullwidth" @click="reset">Reset</a>
+            </p>
+          </b-field>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data: () => ({
-      login: true,
       username: '',
-      password: '',
-      rules: {
-        username: [v => !!v || 'Username is required']
-      }
+      password: ''
     }),
     methods: {
       submit() {
-        if (this.$refs.form.validate()) {
-          this.$session.login(this.username, this.password).then(() => {
-            this.$router.push('/');
-          }).catch(() => {
-            this.login = false;
+        this.$session.login(this.username, this.password).then(() => {
+          this.$router.push('/');
+        }).catch(() => {
+          this.$toast.open({
+            message: 'Invalid username and/or password! Please try again.',
+            type: 'is-warning'
           });
-        }
+        });
       },
       reset() {
-        this.login = true;
         this.$refs.form.reset();
       }
     }
   };
 </script>
+
+<style>
+  .login {
+    width: 400px;
+    left: 50%;
+    margin-left: -200px;
+    margin-top: 200px;
+  }
+</style>
